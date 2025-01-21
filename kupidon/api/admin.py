a09069c0ec55900f2950ba47631d07349e72249d
Вадим Козыревский <v.kozyrevskiy@timeweb.ru>
2025-01-21 13:58:05 +0300

    Initial commit

---
+++ kupidon/api/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+admin.site.site_header = 'Kupidon Administration'
+admin.site.site_title = 'Kupidon'
--- scripts/01_create_user.sql
+++ scripts/01_create_user.sql
@@ -1,1 +1,5 @@
-ALTER ROLE kupidon SUPERUSER;
+-- create the user for test database
+CREATE USER 'kupidon'@'%' IDENTIFIED BY 'kupidon';
+GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `kupidon`.* TO 'kupidon'@'%';
+
+FLUSH PRIVILEGES;
