96b4ffd584fb97885b8b00c6a66dd4b417d29c34
Вадим Козыревский <v.kozyrevskiy@timeweb.ru>
2025-01-21 14:10:55 +0300

    Move secret key into env variables

--- kupidon/kupidon/settings.py
+++ kupidon/kupidon/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3gd@*l0pc5a3*%v(b^2xtlu1%1mywyvschw_r9886tfj$_=dc9'
+SECRET_KEY = config('API_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
